092c503a247ccd63a14dff52c1755e6073bbdf35
zhangheng <zhe_zhangheng@163.com>
2022-09-26 11:18:51 +0800

    初始化提交

---
+++ src/main/java/cn/zhe/leetcode/LeetCodeApplication.java
@@ -0,0 +1,16 @@
+package cn.zhe.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhe
+ */
+@SpringBootApplication
+public class LeetCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodeApplication.class, args);
+    }
+
+}
